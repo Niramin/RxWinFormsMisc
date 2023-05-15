4ad954f6edb3cff1140efc9b3f9f7f86906ca189
Shashwat Ratna <shashwatratna@gmail.com>
2023-05-15 23:47:32 +0530

    simple implementation of IObserver

---
+++ RxWinFormsMisc/Models/ConsoleObserver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RxWinFormsMisc.Models
+{
+    public class ConsoleObserver<T> : IObserver<T>
+    {
+        void IObserver<T>.OnCompleted()
+        {
+            Console.WriteLine("Sequence Terminated");
+        }
+
+        void IObserver<T>.OnError(Exception error)
+        {
+            Console.WriteLine("Sequence faulted with {0}",error);
+        }
+
+        void IObserver<T>.OnNext(T value)
+        {
+            Console.WriteLine("Received value {0}",value);
+        }
+    }
+}
